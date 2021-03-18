a7c108abe25a6d5700f07c9af325fe4bfbb24e22
ics_home\ananin <anton-iv87@yandex.ru>
2021-03-19 00:10:42 +0500

    Clear

--- src/ember-app/app/routes/simple-test-audit-project-master-obj-e/new.js
+++
@@ -1,7 +0,0 @@
-import EditFormNewRoute from 'ember-flexberry/routes/edit-form-new';
-
-export default EditFormNewRoute.extend({
-  modelProjection: 'MasterObjE',
-  modelName: 'simple-test-audit-project-master-obj',
-  templateName: 'simple-test-audit-project-master-obj-e',
-});
